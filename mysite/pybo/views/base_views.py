f220f59a97a9809232b44e59d0d12ca6cd783280
moon-octo <answlsdnd123@naver.com>
2021-05-06 20:18:33 +0900

    추가기능 : 추천많은 댓글 위로 표시 중 에러발생

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -2,7 +2,8 @@ from django.core.paginator import Paginator
 from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
-from ..models import Question
+from ..forms import AnswerForm
+from ..models import Question, Answer
 
 
 def index(request):
@@ -43,6 +44,26 @@ def detail(request, question_id):
     """
     pybo 내용 출력
     """
+    #page_b = request.GET.get('page', '1')  # 페이지
+    so_answer = request.GET.get('so_answer', 'recent')  # 정렬기준
     question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
+    
+    # 정렬
+    if so_answer == 'recommend':
+        answer_list = Answer.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so_answer == 'popular':
+        answer_list = Answer.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        answer_list = Answer.objects.filter(question=question).order_by('-create_date')
+    
+    #paginator_b=Paginator(answer_list, 10)
+    #page_obj_b=paginator_b.get_page(page_b)
+
+
+    context = {'question': answer_list, 'so_answer' : so_answer}
+    #context={'answer_list': page_obj_b, 'page': page_b,'so_answer' : so_answer}
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question, 'so' : so}
+    """
     return render(request, 'pybo/question_detail.html', context)
